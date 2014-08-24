755178edfe4bf26ed38ac1fa0e5469da418bb8b9
Marcos Lopez <feirlane@gmail.com>
2014-08-24 14:27:28 +0200

    Just Arduino Things

--- actuator/actuator/PanTilt.h
+++ actuator/actuator/PanTilt.h
@@ -1,111 +1,135 @@
 #ifndef _PANTILT_H_
 #define _PANTILT_H_
 
-#include "Arduino.h"
+#include <Arduino.h>
 #include <Servo.h>
-
-#define MAX_PAN 180
-#define MIN_PAN 10
-
-#define MAX_TILT 115
-#define MIN_TILT 45
 
 class PanTilt
 {
-  private:
+private:
     float m_pan;
     float m_panSpeed;
     float m_tilt;
     float m_tiltSpeed;
     
+    int m_maxPan;
+    int m_minPan;
+    int m_maxTilt;
+    int m_minTilt;
+    
     long long m_lastUpdate;
     
     Servo *m_panServo;
     Servo *m_tiltServo;
-  
-  public:
+
+public:
     PanTilt(int panPin, int tiltPin)
     {
-      m_lastUpdate = micros();
-      m_pan = 100;
-      m_panSpeed = 0;
-      m_tilt = 100;
-      m_tiltSpeed = 0;
-      
-      
-      Serial.print("Creando panTilt en pins ");
-      Serial.print(panPin);
-      Serial.print(" y ");
-      Serial.println(tiltPin);
-      
-      m_panServo = new Servo();
-      m_panServo->attach(panPin);
-      
-      m_tiltServo = new Servo();
-      m_tiltServo->attach(tiltPin);
-      
-      moveTo(m_pan, m_tilt);
+        m_lastUpdate = micros();
+        m_pan = 90;
+        m_panSpeed = 0;
+        m_tilt = 90;
+        m_tiltSpeed = 0;
+
+
+        Serial.print("Creando panTilt en pins ");
+        Serial.print(panPin);
+        Serial.print(" y ");
+        Serial.println(tiltPin);
+
+        m_panServo = new Servo();
+        m_panServo->attach(panPin);
+
+        m_tiltServo = new Servo();
+        m_tiltServo->attach(tiltPin);
+
+        moveTo(m_pan, m_tilt);
     }
     
     char update()
     {
-      if ((micros() - m_lastUpdate) > 16000)
-      {
-        m_lastUpdate = micros();
-        m_pan = m_pan + m_panSpeed;
-        m_tilt = m_tilt + m_tiltSpeed;
-        
-        if (m_pan > MAX_PAN)
-            m_pan = MAX_PAN;
-          else if (m_pan < MIN_PAN)
-            m_pan = MIN_PAN;
-          
-          if (m_tilt > MAX_TILT)
-            m_tilt = MAX_TILT;
-          else if (m_tilt < MIN_TILT)
-            m_tilt = MIN_TILT;
-        
-        m_panServo->write(m_pan);
-        m_tiltServo->write(m_tilt);
-      }
-      
-      return 0;
+        if ((micros() - m_lastUpdate) > 16000)
+        {
+            m_lastUpdate = micros();
+            m_pan = m_pan + m_panSpeed;
+            m_tilt = m_tilt + m_tiltSpeed;
+
+            if (m_pan > m_maxPan)
+                m_pan = m_maxPan;
+            else if (m_pan < m_minPan)
+                m_pan = m_minPan;
+
+            if (m_tilt > m_maxTilt)
+                m_tilt = m_maxTilt;
+            else if (m_tilt < m_minTilt)
+                m_tilt = m_minTilt;
+
+            m_panServo->write(m_pan);
+            m_tiltServo->write(m_tilt);
+        }
+
+        return 0;
     }
     
     char moveTo(unsigned char X, unsigned char Y)
     {
-      m_pan = X;
-      m_tilt = Y;
-      m_panSpeed = 0;
-      m_tiltSpeed = 0;
-      m_panServo->write(m_pan);
-      m_tiltServo->write(m_tilt);
-      
-      return 0;
+        m_pan = X;
+        m_tilt = Y;
+        m_panSpeed = 0;
+        m_tiltSpeed = 0;
+        m_panServo->write(m_pan);
+        m_tiltServo->write(m_tilt);
+
+        return 0;
     }
     
     char accelerate(char dX, char dY)
     {
-      float maxSpeed = 3;
+        float maxSpeed = 3;
         
-      int realX = (dX * maxSpeed) / 128;
-      int realY = (dY * maxSpeed) / 128;
-      
-      m_panSpeed = -realX;
-      m_tiltSpeed = realY;
-      
-      return 0;
+        float realX = (dX * maxSpeed) / (float)128;
+        float realY = (dY * maxSpeed) / (float)128;
+
+        m_panSpeed = -realX;
+        m_tiltSpeed = realY;
+        /*
+      Serial.print("New Speed: ");
+      Serial.print(m_panSpeed);
+      Serial.print(", ");
+      Serial.println(m_tiltSpeed);
+      */
+        return 0;
     }
     char* limits()
     {
-      char bytes[5] = { MIN_PAN, MAX_PAN, MIN_TILT, MAX_TILT, 0x00 };
-      return bytes;
+        char bytes[5] = { m_minPan, m_maxPan, m_minTilt, m_maxTilt, 0x00 };
+        return bytes;
     }
     
     char* currentPosition()
     {
-      char bytes[3] = { -m_pan, m_tilt, 0x00 };
-      return bytes;
+        char bytes[3] = { m_pan, m_tilt, 0x00 };
+        return bytes;
+    }
+    
+    void setMinPan(int minPan)
+    {
+        m_minPan = minPan;
+    }
+    
+    void setMaxPan(int maxPan)
+    {
+        m_maxPan = maxPan;
+    }
+    
+    void setMinTilt(int minTilt)
+    {
+        m_minTilt = minTilt;
+    }
+    
+    void setMaxTilt(int maxTilt)
+    {
+        m_maxTilt = maxTilt;
     }
 };
 
--- actuator/actuator/actuator.ino
+++ actuator/actuator/actuator.ino
@@ -1,5 +1,8 @@
+#include <Arduino.h>
 #include <Servo.h>
 #include "PanTilt.h"
+
+#define __DEBUG__ 1
 
 #define LASER_PIN 6
 #define LASER_PIN_2 7
@@ -9,7 +12,7 @@
 char buf[BUF_SIZE];
 char bufLen = 0;
 
-PanTilt* panTilts[1];
+PanTilt* panTilts[2];
 int nPanTilts = 0;
 
 Servo servoPan, servoTilt;
@@ -18,7 +21,7 @@ void serialWrite(char bytes[]);
 void ack();
 void nack();
 void processCommand();
-void accelerate(unsigned char panTiltId, unsigned char dX, unsigned char dY);
+void accelerate(unsigned char panTiltId, char dX, char dY);
 void moveTo(unsigned char panTiltId, char X, char Y);
 void limits(unsigned char panTiltId);
 void getPosition(unsigned char panTiltId);
@@ -27,231 +30,244 @@ void stopFiring(unsigned char gunId);
 
 void setup()
 {
-  Serial.begin(19200);
-  Serial1.begin(19200);
+    Serial.begin(19200);
+    Serial1.begin(19200);
 
-  panTilts[0] = new PanTilt(2,3);
-  panTilts[1] = new PanTilt(4,5);
-  nPanTilts = 2;
-  
-  pinMode(LASER_PIN, OUTPUT);
-  digitalWrite(LASER_PIN, LOW);
-  
-  pinMode(LASER_PIN_2, OUTPUT);
-  digitalWrite(LASER_PIN_2, LOW);
+    PanTilt* pt;
+    pt = new PanTilt(2,3);
+    pt->setMinPan(10);
+    pt->setMaxPan(180);
+    pt->setMinTilt(45);
+    pt->setMaxTilt(110);
+    panTilts[0] = pt;
+
+    pt = new PanTilt(8,9);
+    pt->setMinPan(0);
+    pt->setMaxPan(0xb3);
+    pt->setMinTilt(0);
+    pt->setMaxTilt(255);
+    panTilts[1] = pt;
+    nPanTilts = 2;
+
+    pinMode(LASER_PIN, OUTPUT);
+    digitalWrite(LASER_PIN, LOW);
+
+    pinMode(LASER_PIN_2, OUTPUT);
+    digitalWrite(LASER_PIN_2, LOW);
 }
 
 void loop()
 {
-  int i;
-  for (i=0; i<nPanTilts; ++i)
-  {
-    panTilts[i]->update();
-  }
+    int i;
+    for (i=0; i<nPanTilts; ++i)
+    {
+        panTilts[i]->update();
+    }
 }
 
 void serialEvent1()
 {
-	while (Serial1.available())
-        {
-		unsigned char myByte = Serial1.read();
+    while (Serial1.available())
+    {
+        unsigned char myByte = Serial1.read();
 
-                Serial.print("Received: ");
-                Serial.println(myByte, HEX);
-		
-		/* If we receive the begining of a new command
-		 * while reading another one, reset the buffer and ignore
-		 * the previous command
-		 */
-		if (myByte == 0x02)
-                {
-			bufLen = 0;
-		}
-		
-		buf[bufLen] = myByte;
-		bufLen++;
-		if (myByte == 0x03)
-                {
-			processCommand();
-                        break;
-			bufLen = 0;
-		}
-		
-		if (bufLen >= BUF_SIZE)
-                {
-			bufLen = 0;
-		}
-	}
+        Serial.print("Received: ");
+        Serial.println(myByte, HEX);
+
+
+        /* If we receive the begining of a new command
+         * while reading another one, reset the buffer and ignore
+         * the previous command
+         */
+        if (myByte == 0x02)
+        {
+            bufLen = 0;
+        }
+
+        buf[bufLen] = myByte;
+        bufLen++;
+        if (myByte == 0x03)
+        {
+            processCommand();
+            break;
+            bufLen = 0;
+        }
+
+        if (bufLen >= BUF_SIZE)
+        {
+            bufLen = 0;
+        }
+    }
 }
 
 void processCommand()
 {
-	if (buf[0] == 0x02 && bufLen > 1)
+    if (buf[0] == 0x02 && bufLen > 1)
+    {
+        switch (buf[1])
         {
-		switch (buf[1])
-                {
-		case 'L':
-			Serial.println("Limits");
-                        if  (bufLen >= 3)
-                        {
-                          limits(buf[2]);
-                        }
-                        else
-                        {
-                          nack();
-                        }
-			break;
-		case 'A':
-			Serial.println("Absolute");
-                        if (bufLen >= 6)
-                        {
-                          moveTo(buf[2], buf[3], buf[4]);
-                        }
-			break;
-		case 'M':
-                        Serial.println("Relative");
-                        if (bufLen >= 6)
-                        {
-			  accelerate(buf[2], buf[3], buf[4]);
-                        }
-                        else
-                        {
-                          Serial.println("Invalid message length");
-                          nack();
-                        }
-			break;
-		case 'P':
-			Serial.println("Position");
-                        if (bufLen >= 3)
-                        {
-                          getPosition(buf[2]);
-                        }
-                        else
-                        {
-                          Serial.println("Invalid number of parameters");
-                          nack();
-                        }
-			break;
-		case 'S':
-			Serial.println("Pew");
-                        if (bufLen >= 3)
-                        {
-                          Serial.println("Pew Pew");
-                          fire(buf[2]);
-                        }
-			break;
-		case 'H':
-			Serial.println("No pew");
-                        if (bufLen >= 3)
-                        {
-                          stopFiring(buf[2]);
-                        }
-			break;
-		default:
-			Serial.print("Unknown command: ");
-			Serial.println(buf[1]);
-			break;
-		}
-	}
+        case 'L':
+            Serial.println("Limits");
+            if  (bufLen >= 3)
+            {
+                limits(buf[2]);
+            }
+            else
+            {
+                nack();
+            }
+            break;
+        case 'A':
+            Serial.println("Absolute");
+            if (bufLen >= 6)
+            {
+                moveTo(buf[2], buf[3], buf[4]);
+            }
+            break;
+        case 'M':
+            Serial.println("Relative");
+            if (bufLen >= 6)
+            {
+                accelerate(buf[2], buf[3], buf[4]);
+            }
+            else
+            {
+                Serial.println("Invalid message length");
+                nack();
+            }
+            break;
+        case 'P':
+            Serial.println("Position");
+            if (bufLen >= 3)
+            {
+                getPosition(buf[2]);
+            }
+            else
+            {
+                Serial.println("Invalid number of parameters");
+                nack();
+            }
+            break;
+        case 'S':
+            Serial.println("Pew");
+            if (bufLen >= 3)
+            {
+                fire(buf[2]);
+            }
+            break;
+        case 'H':
+            Serial.println("No pew");
+            if (bufLen >= 3)
+            {
+                stopFiring(buf[2]);
+            }
+            break;
+        default:
+            Serial.print("Unknown command: ");
+            Serial.println(buf[1]);
+            break;
+        }
+    }
 }
 
 void accelerate(unsigned char panTiltId, char dX, char dY)
 {
-  if (panTiltId < nPanTilts)
-  {
+    if (panTiltId < nPanTilts)
+    {
+        /*
     Serial.print("Accelerate: ");
     Serial.print(panTiltId);
     Serial.print(", ");
     Serial.print(dX);
     Serial.print(", ");
-    Serial.println(dY);
-    if (!panTilts[panTiltId]->accelerate(dX, dY))
-    {
-      //ack();
-      return;
+    Serial.println(dY);*/
+        if (!panTilts[panTiltId]->accelerate(dX, dY))
+        {
+            //ack();
+            return;
+        }
     }
-  }
-  nack();
+    nack();
 }
 
 void moveTo(unsigned char panTiltId, char X, char Y)
 {
-  if (panTiltId < nPanTilts)
-  {
-    panTilts[panTiltId]->moveTo(X, Y);
-  }
+    if (panTiltId < nPanTilts)
+    {
+        panTilts[panTiltId]->moveTo(X, Y);
+    }
 }
 
 void limits(unsigned char panTiltId)
 {
-  if (panTiltId < nPanTilts)
-  {
-    serialWrite(panTilts[panTiltId]->limits());
-  }
-  else
-  {
-    nack();
-  }
+    if (panTiltId < nPanTilts)
+    {
+        serialWrite(panTilts[panTiltId]->limits());
+    }
+    else
+    {
+        nack();
+    }
 }
 void getPosition(unsigned char panTiltId)
 {
-  if (panTiltId < nPanTilts)
-  {
-    serialWrite(panTilts[panTiltId]->currentPosition());
-  }
-  else
-  {
-    nack();
-  }
+    if (panTiltId < nPanTilts)
+    {
+        serialWrite(panTilts[panTiltId]->currentPosition());
+    }
+    else
+    {
+        nack();
+    }
 }
 
 void fire(unsigned char gunId)
 {
-  if (gunId == 0)
-  {
-    digitalWrite(LASER_PIN, HIGH);
-  }
-  else
-  {
-    digitalWrite(LASER_PIN_2, HIGH);
-  }
+    if (gunId == 0)
+    {
+        digitalWrite(LASER_PIN, HIGH);
+    }
+    else
+    {
+        digitalWrite(LASER_PIN_2, HIGH);
+    }
 }
 
 void stopFiring(unsigned char gunId)
 {
-  if (gunId == 0)
-  {
-    digitalWrite(LASER_PIN, LOW);
-  }
-  else
-  {
-    digitalWrite(LASER_PIN_2, LOW);
-  }
+    if (gunId == 0)
+    {
+        digitalWrite(LASER_PIN, LOW);
+    }
+    else
+    {
+        digitalWrite(LASER_PIN_2, LOW);
+    }
 }
 
 void ack()
 {
-  char bytes[] = {0x06, 0x00 };
-  //serialWrite(bytes);
+    char bytes[] = {0x06, 0x00 };
+    //serialWrite(bytes);
 }
 
 void nack()
 {
-  char bytes[] = { 0x15, 0x00 };
-  //serialWrite(bytes);
+    char bytes[] = { 0x15, 0x00 };
+    //serialWrite(bytes);
 }
 
 void serialWrite(char bytes[])
 {
-  unsigned char idx = 0;
-  unsigned char thisByte = bytes[idx++];
-  Serial1.write(0x02);
-  while (thisByte != 0)
-  {
-    Serial1.write(thisByte);
-    thisByte = bytes[idx++];
-  }
-  Serial1.write(0x03);
-  
-  
+    unsigned char idx = 0;
+    unsigned char thisByte = bytes[idx++];
+    Serial1.write(0x02);
+    while (thisByte != 0)
+    {
+        Serial1.write(thisByte);
+        thisByte = bytes[idx++];
+    }
+    Serial1.write(0x03);
+
+
 }
